8d94e0f20dcf9ca84ec16222826b5350ef695261
Matthew Cheung <n10493077@qut.edu.au>
2024-03-05 14:27:41 +1000

    test again

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -2,7 +2,7 @@ public class ToDoItem {
     private String description;
     private boolean isDone;
 
-    private int test
+    private int test;
     // TODO Now:
     // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
     // Add getters and setters for each field
